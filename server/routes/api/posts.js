0f099789927d9cc6210792485471063f508458cd
Szeszák Ádám <szeszak.adam@gmail.com>
2020-07-25 22:51:30 +0200

    initial background created

---
+++ server/routes/api/posts.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongodb = require('mongodb');
+
+const router = express.Router();
+
+//Get posts
+router.get('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    res.send(await posts.find({}).toArray()); 
+});
+
+//Add posts
+router.post('/', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.insertOne({
+        text: req.body.text,
+        createdAt: new Date()
+    });
+    res.status(201).send();
+});
+//delete posts
+router.delete('/:id', async (req, res) => {
+    const posts = await loadPostsCollection();
+    await posts.deleteOne({_id: new mongodb.ObjectID(req.params.id)});
+    res.status(200).send();
+});
+
+async function loadPostsCollection() {
+    const client = await mongodb.MongoClient.connect
+    ('mongodb+srv://admin:admin@webtech2.ttqks.mongodb.net/Webtech2?retryWrites=true&w=majority', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    });
+    return client.db('Webtech2').collection('posts');
+}
+
+
+module.exports = router;
